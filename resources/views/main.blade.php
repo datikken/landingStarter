98a98ce1f08dba304823d303c733b52129198e43
nshadybekov <nshadybekov@adsterra.com>
2021-12-25 00:07:46 +0300

    navbar hero

--- resources/views/main.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('layouts.app')
-
-@include('components.navbar')
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
