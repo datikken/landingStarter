9333c60d39c3da950b5ddb8b2f6d389b405461e7
nshadybekov <nshadybekov@adsterra.com>
2021-12-24 19:09:33 +0300

    initial

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav aria-label="Jump links" class="sticky top-0 z-40 bg-white text-sm font-medium text-gray-900 py-5 ring-1 ring-gray-900 ring-opacity-5 shadow-sm">
+  <div class="max-w-container mx-auto px-4 sm:px-6 lg:px-8 flex space-x-5 sm:space-x-10 lg:space-x-14">
+    <div class="logo">
+        <div class="logo-image"></div>
+    </div>
+    <a href="#product-application-ui">Application UI</a>
+    <a href="#product-ecommerce" class="flex items-center">
+      Ecommerce<span class="sr-only hidden sm:inline"> - </span><span class="hidden sm:inline ml-3 text-xs font-semibold p-1.5 -my-1 bg-teal-50 text-teal-700 rounded-md">New</span>
+    </a>
+  </div>
+</nav>
+
