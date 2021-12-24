3e3d515678f6a214028046a484a12885180f9201
nshadybekov <nshadybekov@adsterra.com>
2021-12-24 22:07:21 +0300

    tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
-    .css('resources/css/app.css', 'public/css')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
     .sourceMaps();
